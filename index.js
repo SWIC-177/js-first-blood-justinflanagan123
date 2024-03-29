d40c3ea033294bca355d4625bea4eafd03353be8
justinflangan123 <justin.flanagan@student.swic.edu>
2024-03-29 01:17:01 -0500

    Add variables userName and UserAge

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ function verifyAdulthood(personName, personAge) {
   return `You are not old enough to enter, ${personName}.`;
 }
 
+const userName = person.name;
+const userAge = person.age;
+
 const adultMessage = verifyAdulthood(userName, userAge);
 
 console.log(adultMessage);
