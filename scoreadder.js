a698b1076e2a12490dbce9117e585024744a5f2f
justinflangan123 <justin.flanagan@student.swic.edu>
2024-04-07 00:59:21 -0500

    Test basketball scoring

--- scoreadder.js
+++ scoreadder.js
@@ -9,3 +9,8 @@ const addThreePoints = createScoreAdder(3);
 const addTwoPointsFootball = createScoreAdder(2);
 const addThreePointsFootball = createScoreAdder(3);
 const addSixPointsFootball = createScoreAdder(6);
+
+console.log("Basketball Scores:");
+console.log(addOnePoint(1)); // Output: 1
+console.log(addTwoPoints(2)); // Output: 4
+console.log(addThreePoints(3)); // Output: 9
