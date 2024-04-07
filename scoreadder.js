1049aebadbd970fb7cd3c16913f467d3a5342bec
justinflangan123 <justin.flanagan@student.swic.edu>
2024-04-07 00:58:57 -0500

    Add football scoring examples

--- scoreadder.js
+++ scoreadder.js
@@ -5,3 +5,7 @@ function createScoreAdder(score) {
 const addOnePoint = createScoreAdder(1);
 const addTwoPoints = createScoreAdder(2);
 const addThreePoints = createScoreAdder(3);
+
+const addTwoPointsFootball = createScoreAdder(2);
+const addThreePointsFootball = createScoreAdder(3);
+const addSixPointsFootball = createScoreAdder(6);
