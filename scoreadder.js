b1ea4501c5cac14384aa429a2388205d254517ab
justinflangan123 <justin.flanagan@student.swic.edu>
2024-04-07 00:56:17 -0500

    Add basketball scoring examples

--- scoreadder.js
+++ scoreadder.js
@@ -1,3 +1,7 @@
 function createScoreAdder(score) {
   return (increment) => increment * score;
 }
+
+const addOnePoint = createScoreAdder(1);
+const addTwoPoints = createScoreAdder(2);
+const addThreePoints = createScoreAdder(3);
